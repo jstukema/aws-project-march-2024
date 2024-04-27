e9f1c25a43f04eadf3a6e298cf345d30ad5f075b
Etuk S Jeremiah <jsetukema@gmail.com>
2024-04-27 11:52:20 +0100

    Updated my csv file on aws to handle uft-8 encoding

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import dotenv
 from dotenv import load_dotenv
 import os
 import boto3
@@ -8,7 +7,32 @@ import boto3
 env = load_dotenv()
 
 
-s3 = boto3.resource('s3', aws_access_key_id=os.getenv("access_Key"),
+# s3 = boto3.resource('s3', aws_access_key_id=os.getenv("access_Key"),
+#                                  aws_secret_access_key=os.getenv("secret_access_key"))
+# for bucket in s3.buckets.all():
+#     print(bucket.name)
+
+client = boto3.client('s3', aws_access_key_id=os.getenv("access_Key"),
                                  aws_secret_access_key=os.getenv("secret_access_key"))
-for bucket in s3.buckets.all():
-    print(bucket.name)
+
+
+response = client.get_object(Bucket="data-analytics-p", Key="DWP Pension Regulator/Data Analyst Interview Task - 2024.csv")
+
+print(response.keys())
+
+status = response.get('ResponseMetadata', {}).get('HTTPStatusCode', {})
+
+if status == 200:
+
+    print(f"Successfully pulled the data: status - {status}")
+    # Read the CSV using different encodings
+    encodings_to_try = ['utf-8', 'latin-1', 'unicode_escape']
+    for encoding in encodings_to_try:
+        try:
+            df = pd.read_csv(response['Body'], encoding=encoding)
+            print("Successfully decoded using encoding:", encoding)
+            break  # Stop trying encodings if successful
+        except UnicodeDecodeError:
+            print("Failed to decode using encoding:", encoding)
+else:
+    print(f"Unable to pull the data: status - {status}")
